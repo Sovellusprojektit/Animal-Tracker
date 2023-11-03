cf46f521a1c064c63f88c8b7088a960cc23e050d
unknown <antti.lassila98@gmail.com>
2023-11-03 10:34:50 +0200

    Korjattu väärä datatyyppi RestControlleriin

--- backend/src/main/java/com/server/backend/controller/DataController.java
+++ backend/src/main/java/com/server/backend/controller/DataController.java
@@ -1,9 +1,13 @@
 package com.server.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.server.backend.data.User;
 import com.server.backend.service.UserService;
 
 
@@ -18,8 +22,8 @@ public class DataController {
 
 
     @GetMapping("/users")
-    public String getAllUsers() {
-        return userService.getAllUsers().toString();
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
     }
     
 }
