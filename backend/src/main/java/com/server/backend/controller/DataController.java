41b1791883aa660ec75914e61e6a8895caee6b53
unknown <antti.lassila98@gmail.com>
2023-11-04 17:51:02 +0200

    Server ja database pyörii dockerissa

--- backend/src/main/java/com/server/backend/controller/DataController.java
+++ backend/src/main/java/com/server/backend/controller/DataController.java
@@ -1,13 +1,21 @@
 package com.server.backend.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.server.backend.data.User;
+import com.server.backend.repository.UserRepo;
 import com.server.backend.service.UserService;
 
 
@@ -20,10 +28,37 @@ public class DataController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserRepo userRepo;
+
 
     @GetMapping("/users")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @PostMapping("/adduser")
+    public User addUser(@RequestParam String fname, @RequestParam String lname ,@RequestParam String email, @RequestParam String password) {
+        User user = new User();
+        user.setFname(fname);
+        user.setLname(lname);
+        user.setEmail(email);
+        user.setPassword(password);
+
+        return userRepo.save(user);
+        
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable int id) {
+        Optional<User> user = userRepo.findById(id);
+
+        if(user.isPresent()) {
+            userRepo.deleteById(id);
+            return new ResponseEntity<>("User deleted successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+        }
     }
     
 }
