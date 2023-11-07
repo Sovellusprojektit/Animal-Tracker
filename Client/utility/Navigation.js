3af68df8075264fe6329a341b6568d0f8b125f5f
c1tian <c1tian00@students.oamk.fi>
2023-11-07 14:36:05 +0200

    loginPage viimeistelyä vaille valmis.

--- Client/utility/Navigation.js
+++ Client/utility/Navigation.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import RegisterScreen from '../pages/RegisterScreen';
 import LoginScreen from '../pages/LoginScreen';
@@ -5,10 +6,32 @@ import LoginScreen from '../pages/LoginScreen';
 const Stack = createStackNavigator();
 
 const Navigation = () => {
-    return (
+  return (
     <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen
+        name="Register"
+        component={RegisterScreen}
+        options={{
+          headerTransparent: true,
+          headerBackground: () => null,
+          headerTitleStyle: {
+            color: 'grey',
+            fontSize: 14,
+          },
+        }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{
+          headerTransparent: true,
+          headerBackground: () => null,
+          headerTitleStyle: {
+            color: 'grey',
+            fontSize: 14,
+          },
+        }}
+      />
     </Stack.Navigator>
   );
 }
