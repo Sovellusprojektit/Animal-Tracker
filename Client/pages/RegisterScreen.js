87f22bf2a67608fe4e38f05426248235f2bd2f5d
t1juko00 <t1juko00@students.oamk.fi>
2023-11-07 14:46:38 +0200

    register page valmis ja post pyyntö toimii

--- Client/pages/RegisterScreen.js
+++ Client/pages/RegisterScreen.js
@@ -1,39 +1,36 @@
 import React from 'react';
-import { View, StyleSheet,Button,Alert } from 'react-native';
+import { View, StyleSheet, Button, Alert } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
-
+import axios from 'axios';
+import { IP_ADDRESS } from '@env'
 const RegisterScreen = ({ navigation }) => {
   const [fname, setFname] = React.useState('');
   const [lname, setLname] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [email, setEmail] = React.useState('');
 
-  const handleRegister = () => {
+  const formData = new FormData();
+  formData.append('fname', fname);
+  formData.append('lname', lname);
+  formData.append('password', password);
+  formData.append('email', email);
+
+  const handleRegister = async () => {
     if (email.length < 1 || !email.includes('@')) {
       Alert.alert('Invalid email address');
     } else if (password.length < 6) {
       Alert.alert('Password must be at least 6 characters');
-    } else
-    fetch('http://localhost:8080/adduser', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        fname: fname,
-        lname: lname,
-        password: password,
-        email: email,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
+    } else {
+      try {
+        const result = await axios.post(`http://${IP_ADDRESS}:8080/adduser`, formData);
+        console.log('Response:', result.data);
+        Alert.alert('User registered');
         navigation.navigate('Login');
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+      } catch (error) {
+        console.log('Error:', error);
+        Alert.alert('Error registering user');
+      }
+    }
   };
 
   return (
@@ -48,11 +45,11 @@ const RegisterScreen = ({ navigation }) => {
         placeholder="Last name"
         onChangeText={(text) => setLname(text)}
       ></TextInput>
-        <TextInput
+      <TextInput
         style={styles.textInput}
         placeholder="Email"
         onChangeText={(text) => setEmail(text)}
-        ></TextInput>
+      ></TextInput>
       <TextInput
         style={styles.textInput}
         secureTextEntry={true}
@@ -71,30 +68,29 @@ const RegisterScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: 'orange',
-      flexDirection: 'column', 
-      alignItems: 'center',
-      justifyContent: 'flex-start',
-      paddingTop: 100,
-      flex: 1, 
-    },
-    textInput: {
-      height: 40,
-      borderColor: 'black',
-      borderWidth: 1,
-      width: '80%',
-      padding: 10,
-      marginBottom: 20,
-      borderRadius: 5,
-    },
-    buttonstyle: {
+  container: {
+    backgroundColor: 'orange',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 100,
+    flex: 1,
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    width: '80%',
+    padding: 10,
+    marginBottom: 20,
+    borderRadius: 5,
+  },
+  buttonstyle: {
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 4,
     padding: 10,
-    },
-  });
-  
+  },
+});
 
 export default RegisterScreen;
