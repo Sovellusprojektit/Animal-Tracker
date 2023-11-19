27964c518f9f887bc8f013efd9d6200bcb07dd61
t1juko00 <t1juko00@students.oamk.fi>
2023-11-19 10:59:59 +0200

    maastokartta saatu näkyviin

--- client/utility/Map.js
+++ client/utility/Map.js
@@ -1,21 +1,41 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet } from 'react-native';
 import Mapbox from '@rnmapbox/maps';
 import { MAPBOX_ACCESS_TOKEN } from '@env';
+import {MAANMITTAUSLAITOS_API_KEY} from '@env';
 
 Mapbox.setAccessToken(MAPBOX_ACCESS_TOKEN);
 
 const Map = () => {
+
   return (
     <View style={styles.page}>
       <View style={styles.container}>
-        <Mapbox.MapView style={styles.map} />
+        <Mapbox.MapView 
+            style={styles.map}
+            styleURL={`https://avoin-karttakuva.maanmittauslaitos.fi/vectortiles/stylejson/v20/generic.json?TileMatrixSet=WGS84_Pseudo-Mercator&api-key=${MAANMITTAUSLAITOS_API_KEY}`}
+            centerCoordinate={[24.945831, 60.192059]}
+            zoomLevel={8}
+            showUserLocation={true}
+            logoEnabled={false}
+            >
+          <Mapbox.Camera
+            zoomLevel={8}
+            centerCoordinate={[24.945831, 60.192059]}
+            animationMode={'flyTo'}
+            animationDuration={0}
+
+          />
+            <Mapbox.PointAnnotation
+              id="marker"
+              coordinate={[24.945831, 60.192059]}
+              title="Test"/>
+ 
+        </Mapbox.MapView>
       </View>
     </View>
   );
-}
-
-export default Map;
+};
 
 const styles = StyleSheet.create({
   page: {
@@ -24,11 +44,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   container: {
-    height: 300,
-    width: 300,
-
+    height: 1350,
+    width: 400,
   },
   map: {
-    flex: 2,
-  }
+    flex: 1,
+  },
 });
+
+export default Map;
