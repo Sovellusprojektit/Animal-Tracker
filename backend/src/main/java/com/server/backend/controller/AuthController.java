729f5af27d5c367f30422c6d13421a5182f00b9e
unknown <antti.lassila98@gmail.com>
2023-11-28 19:02:12 +0200

    Korjailtu logiikkaa

--- backend/src/main/java/com/server/backend/controller/AuthController.java
+++ backend/src/main/java/com/server/backend/controller/AuthController.java
@@ -11,7 +11,6 @@ import com.server.backend.security.AuthService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
